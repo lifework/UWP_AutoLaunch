5f597349cf33d5cc3446f6764ec5c4c3600430bc
lifework <kobayashi@lifework.tech>
2022-08-29 22:01:33 +0900

    Install-Package Microsoft.Toolkit.Uwp.Notifications -Version 7.1.2

--- BackgroundTasks/UpdateTask.cs
+++ BackgroundTasks/UpdateTask.cs
@@ -6,12 +6,19 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Background;
 
+
 namespace BackgroundTasks
 {
     public sealed class UpdateTask : IBackgroundTask
     {
         public void Run(IBackgroundTaskInstance taskInstance)
         {
+/*            new ToastContentBuilder()
+                .AddArgument("action", "viewConversation")
+                .AddArgument("conversationId", 9813)
+                .AddText("Andrew sent you a picture")
+                .AddText("Check this out, The Enchantments in Washington!")
+                .Show();*/
             Debug.WriteLine("OK");
         }
     }
